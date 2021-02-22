539b8eaad388a2fa4b9cd7696682e93dc3fbaefd
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2021-02-22 14:29:32 +0000

    changed MainPageHelper

--- src/test/java/com/telran/cheaptrip/pages/MainPageHelper.java
+++ src/test/java/com/telran/cheaptrip/pages/MainPageHelper.java
@@ -104,4 +104,37 @@ public class MainPageHelper extends PageBase {
         letsGoButton.click();
     }
 
+    @FindBy(id = "main-content")
+    WebElement homepage;
+
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.Button")
+    WebElement hamburger;
+
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View")
+    WebElement contacts;
+
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View[1]/android.view.View[3]")
+    WebElement contactData;
+
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[1]/android.view.View/android.view.View[1]/android.view.View[1]/android.view.View[1]/android.widget.ListView/android.widget.Button")
+    WebElement homeButton;
+
+    public void findHamburger() throws InterruptedException {
+        Thread.sleep(8000);
+        hamburger.click();
+        waitUntilElementVisible(contacts, 3);
+    }
+
+    public void findContacts() {
+        contacts.click();
+        waitUntilElementVisible(contactData, 10);
+    }
+
+    public void findHomeButton() throws InterruptedException {
+        Thread.sleep(1000);
+        hamburger.click();
+        homeButton.click();
+        assert isElementPresent(homepage);
+    }
+
 }
